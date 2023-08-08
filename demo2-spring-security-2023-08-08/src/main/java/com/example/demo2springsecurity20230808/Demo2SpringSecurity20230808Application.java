ffac959ce79b8fee195af905a8bc55cc7fca1c3c
roczhang <mr.roczhang@outlook.com>
2023-08-08 18:18:56 +0800

    配置spring security

---
+++ demo2-spring-security-2023-08-08/src/main/java/com/example/demo2springsecurity20230808/Demo2SpringSecurity20230808Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2springsecurity20230808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringSecurity20230808Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2SpringSecurity20230808Application.class, args);
+    }
+
+}
---
+++ demo2-spring-security-2023-08-08/src/test/java/com/example/demo2springsecurity20230808/Demo2SpringSecurity20230808ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2springsecurity20230808;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SpringSecurity20230808ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
