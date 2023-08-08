a06e8f3b723003b81b66a9bd937e48d3aff94f29
roczhang <mr.roczhang@outlook.com>
2023-08-08 16:08:33 +0800

    初始化项目，并添加一个demo

---
+++ demo1_base_2023_08_08/src/main/java/com/example/demo1_base_2023_08_08/Demo1Base20230808Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1_base_2023_08_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Base20230808Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Base20230808Application.class, args);
+    }
+
+}
---
+++ demo1_base_2023_08_08/src/test/java/com/example/demo1_base_2023_08_08/Demo1Base20230808ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1_base_2023_08_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1Base20230808ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
