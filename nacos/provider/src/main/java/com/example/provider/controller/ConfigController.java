dbd825dbed3793fa12d7561649918409b117f205
roczhang <mr.roczhang@outlook.com>
2023-08-21 11:40:21 +0800

    nacos服务发现

--- nacos/provider/src/main/java/com/example/provider/controller/ConfigController.java
+++ nacos/provider/src/main/java/com/example/provider/controller/ConfigController.java
@@ -1,7 +1,27 @@
 package com.example.provider.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 /**
  * @description:
  * @author: roczhang
  * @date: 2023/8/21 10:31
  */
-public class ConfigController {}
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+  @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+  private boolean useLocalCache;
+
+  @RequestMapping(value = "/get", method = GET)
+  @ResponseBody
+  public boolean get() {
+    return useLocalCache;
+  }
+}
