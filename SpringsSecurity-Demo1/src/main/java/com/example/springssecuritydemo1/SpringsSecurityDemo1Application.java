ca6750eaefcc74e57164429a3fae067c10d9685d
roczhang <mr.roczhang@outlook.com>
2023-08-31 17:51:40 +0800

    增加基于gradle的SpringSecurity项目

---
+++ SpringsSecurity-Demo1/src/main/java/com/example/springssecuritydemo1/SpringsSecurityDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springssecuritydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsSecurityDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsSecurityDemo1Application.class, args);
+    }
+
+}
---
+++ SpringsSecurity-Demo1/src/test/java/com/example/springssecuritydemo1/SpringsSecurityDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springssecuritydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsSecurityDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
