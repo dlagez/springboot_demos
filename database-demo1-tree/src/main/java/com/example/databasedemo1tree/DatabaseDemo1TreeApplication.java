726cee2e4dd414d0ed302e483439b682b4f3fb39
roczhang <mr.roczhang@outlook.com>
2023-09-25 17:03:13 +0800

    feat: 新建项目，测试数据库树状结构

---
+++ database-demo1-tree/src/main/java/com/example/databasedemo1tree/DatabaseDemo1TreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasedemo1tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemo1TreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDemo1TreeApplication.class, args);
+    }
+
+}
---
+++ database-demo1-tree/src/test/java/com/example/databasedemo1tree/DatabaseDemo1TreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasedemo1tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemo1TreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
