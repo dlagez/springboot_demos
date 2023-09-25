7cc3e4cc5714331489b8e3ee5eace1e2688264bc
roczhang <mr.roczhang@outlook.com>
2023-09-25 18:30:05 +0800

    refactor: 配置数据库

--- database-demo1-tree/src/main/java/com/example/databasedemo1tree/DatabaseDemo1TreeApplication.java
+++ database-demo1-tree/src/main/java/com/example/databasedemo1tree/DatabaseDemo1TreeApplication.java
@@ -1,9 +1,11 @@
 package com.example.databasedemo1tree;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.databasedemo1tree.mapper")
 public class DatabaseDemo1TreeApplication {
 
     public static void main(String[] args) {
