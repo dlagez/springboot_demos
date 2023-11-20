9fbb9dbfdf9fe85f61ddefc360dc5c49854e5038
roczhang <mr.roczhang@outlook.com>
2023-11-20 08:41:31 +0800

    feat: nacos config test2

--- spring-cloud-nacos-1/src/main/java/com/example/springcloudnacos1/controller/TestController.java
+++ spring-cloud-nacos-1/src/main/java/com/example/springcloudnacos1/controller/TestController.java
@@ -1,13 +1,7 @@
 package com.example.springcloudnacos1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.springcloudnacos1.model.NacosConfigInfo;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author: roczhang
@@ -17,21 +11,10 @@ import java.util.Map;
 @RequestMapping("/nacos/bean")
 public class TestController {
 
-    @Autowired
-    private NacosConfigInfo nacosConfigInfo;
 
     @RequestMapping("/get")
     public String get() {
         return "Hello Nacos Config!";
     }
 
-    @GetMapping
-    public Map<String, String> getConfigInfo() {
-        Map<String, String> result = new HashMap<>();
-        result.put("serverAddr", nacosConfigInfo.getServerAddr());
-        result.put("prefix", nacosConfigInfo.getPrefix());
-        result.put("group", nacosConfigInfo.getGroup());
-        result.put("namespace", nacosConfigInfo.getNamespace());
-        return result;
-    }
 }
