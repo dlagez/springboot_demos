7a4c159b231d0219651c4d13801ee02c0b45dd9d
roczhang <mr.roczhang@outlook.com>
2024-03-08 14:16:05 +0800

    feat: nacos config 设置config

--- spring-cloud-nacos-1/src/main/java/com/example/springcloudnacos1/controller/TestController.java
+++ spring-cloud-nacos-1/src/main/java/com/example/springcloudnacos1/controller/TestController.java
@@ -1,20 +1,25 @@
 package com.example.springcloudnacos1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author: roczhang
- * @date: 2023/11/18 11:18
+ * @author roczhang
+ * @date 2023/11/18 11:18
  */
 @RestController
 @RequestMapping("/nacos/bean")
+@RefreshScope
 public class TestController {
 
+    @Value("${name:roczhang-local}")
+    private String useLocalCache;
 
     @RequestMapping("/get")
     public String get() {
-        return "Hello Nacos Config!";
+        return useLocalCache;
     }
 
 }
